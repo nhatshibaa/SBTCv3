e49788dfe440987290276ea52487358a5ea1303a
Nhật Nguyễn Tiến <kaotuong113@gmail.com>
2022-04-03 18:36:42 +0700

    Add project files.

---
+++ SBTCv3/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SBTCv3.Models;
+using System.Diagnostics;
+
+namespace SBTCv3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult about()
+        {
+            return View();
+        }
+        public IActionResult contact()
+        {
+            return View();
+        }
+        public IActionResult faq()
+        {
+            return View();
+        }
+
+        public IActionResult term()
+        {
+            return View();
+        }
+        public IActionResult blog()
+        {
+            return View();
+        }
+        public IActionResult travel()
+        {
+            return View();
+        }
+
+        public IActionResult ticket()
+        {
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SBTCv3/Controllers/SendMailController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using SBTCv3.Models;
+using MailKit.Net.Smtp;
+
+
+namespace SBTCv3.Controllers
+{
+    public class SendMailController : Controller
+    {
+        private readonly Models.Mail.MailSettings _mailSettings;
+        public IActionResult SendMail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMail(IMail mail, string userEmail)
+        {
+            try
+            {
+                MimeMessage emailMessage = new MimeMessage();
+
+
+                MailboxAddress emailFrom = new MailboxAddress(_mailSettings.DisplayName, _mailSettings.Mail);
+                emailMessage.From.Add(emailFrom);
+
+                MailboxAddress emailTo = new MailboxAddress(mail.username, mail.email);
+                emailMessage.To.Add(emailTo);
+
+                emailMessage.Subject = "Thông báo đặt vé thành công!";
+
+                string FilePath = Directory.GetCurrentDirectory() + "\\EmailTemplate\\MailTemp.html";
+                string EmailTemplateText = System.IO.File.ReadAllText(FilePath);
+
+
+                EmailTemplateText = string.Format(EmailTemplateText, userEmail);
+
+                BodyBuilder emailBodyBuilder = new BodyBuilder();
+                emailBodyBuilder.HtmlBody = EmailTemplateText;
+                emailMessage.Body = emailBodyBuilder.ToMessageBody();
+
+                SmtpClient emailClient = new SmtpClient();
+                emailClient.Connect(_mailSettings.Host, _mailSettings.Port);
+                emailClient.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+                emailClient.Send(emailMessage);
+                emailClient.Disconnect(true);
+                emailClient.Dispose();                
+            }
+            catch (Exception ex)
+            {
+                //Log Exception Details
+                return Ok(ex);
+            }
+
+            return View();
+        }
+    }
+}
